0c0ec64c0af15fdbfbe9ecbb25100e814f96a7d2
Isaac <isaac.g.hawkins@gmail.com>
2018-10-17 09:10:05 +1100

    Added Score Saving

--- app/src/main/java/au/com/codycodes/tpk/tamizhpallikoodam/QuestionActivity.java
+++ app/src/main/java/au/com/codycodes/tpk/tamizhpallikoodam/QuestionActivity.java
@@ -1,8 +1,11 @@
 package au.com.codycodes.tpk.tamizhpallikoodam;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Button;
@@ -16,20 +19,32 @@ public class QuestionActivity extends AppCompatActivity {
 
     private String correct;
     private int Score = 0;
+
+    static int PREFERENCE_MODE_PRIVATE = 0;
+    private SharedPreferences topscore;
+    private SharedPreferences.Editor topscoreEditior;
+    private int TopScore = 0;
+    public static final String MyPREFERENCES = "myprefs";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question);
-
+        topscore = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
         DatabaseHelper db = new DatabaseHelper(this);
-
+        GetTopScore();
         final Quiz quiz = db.getQuiz();
         Intent myIntent = getIntent();
         Score = myIntent.getIntExtra("score", 0);
+        CheckTopScore();
+
         TextView category = findViewById(R.id.category);
         category.setText(quiz.getCategory());
         TextView score = findViewById(R.id.scoreView);
         score.setText(Integer.toString(Score));
+        TextView Highscore = findViewById(R.id.HighScoreView);
+        Highscore.setText(Integer.toString(TopScore));
         TextView question = findViewById(R.id.question);
         question.setText(quiz.getTamilTranslation());
         TextView option1 = findViewById(R.id.option1);
@@ -40,7 +55,7 @@ public class QuestionActivity extends AppCompatActivity {
         option3.setText(quiz.getOption3());
         TextView option4 = findViewById(R.id.option4);
         option4.setText(quiz.getOption4());
-
+        Log.d("Answer:", quiz.getAnswer());
         final Button submit = findViewById(R.id.submit);
         submit.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -73,5 +88,22 @@ public class QuestionActivity extends AppCompatActivity {
                 mp.start();
             }
         });
+    }
+
+    private void CheckTopScore()
+    {
+        if (Score > TopScore)
+        {
+            topscoreEditior = topscore.edit();
+            TopScore = Score;
+            topscoreEditior.putInt("Score", TopScore);
+            topscoreEditior.apply();
+        }
+    }
+
+    private void GetTopScore()
+    {
+        topscore = getPreferences(PREFERENCE_MODE_PRIVATE);
+        TopScore = topscore.getInt("Score", TopScore);
     }
 }
